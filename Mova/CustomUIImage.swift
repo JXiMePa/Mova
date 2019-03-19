272d444996ebf17cc34adf4afc8aa6fdead22b19
Юрій Тарасенко <jurikfav0rit@gmail.com>
2019-03-19 23:15:39 +0200

    .

--- Mova/CustomUIImage.swift
+++ Mova/CustomUIImage.swift
@@ -34,11 +34,15 @@ final class CustomUIImage: UIImageView {
         }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        addSubview(spiner)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(spiner)
         _ = spiner.anchor(centerX: self.centerXAnchor, centerY: self.centerYAnchor)
         spiner.startAnimating()
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
